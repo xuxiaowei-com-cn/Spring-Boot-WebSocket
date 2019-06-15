0930e9eaa93c8d87d0a139db6cae2fdbd99c55c9
徐晓伟 <xuxiaowei@live.hk>
2019-06-15 23:05:41 +0800

    注释

--- src/main/java/cn/com/xuxiaowei/websocket/WebSocketApplication.java
+++ src/main/java/cn/com/xuxiaowei/websocket/WebSocketApplication.java
@@ -3,11 +3,16 @@ package cn.com.xuxiaowei.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ */
 @SpringBootApplication
-public class SpringBootWebSocketApplication {
+public class WebSocketApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootWebSocketApplication.class, args);
+        SpringApplication.run(WebSocketApplication.class, args);
     }
 
 }
--- src/test/java/cn/com/xuxiaowei/websocket/WebSocketApplicationTests.java
+++ src/test/java/cn/com/xuxiaowei/websocket/WebSocketApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootWebSocketApplicationTests {
+public class WebSocketApplicationTests {
 
     @Test
     public void contextLoads() {
