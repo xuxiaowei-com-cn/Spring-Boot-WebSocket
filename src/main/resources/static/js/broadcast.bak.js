de05a747ed1036bf85fb2f1aeffaadb136012a28
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-08-06 02:39:47 +0800

    bak（未完待续...）

--- src/main/resources/static/js/broadcast.bak.js
+++ src/main/resources/static/js/broadcast.bak.js
@@ -19,9 +19,14 @@ function connect() {
     stompClient.debug = false; // 禁用控制台打印你连接时的内容
 
     // 连接 WebSocket 服务端
-    stompClient.connect({}, function (frame) {
+    stompClient.connect({}, function (frame) { // 连接
+
+        $("#isConnect").show();
+        $("#connect").hide();
+        $("#disconnect").show();
 
         setConnected(true); // 设置已连接
+
         console.log('Stomp SockJS 已连接');
 
         // 通过 stompClient.subscribe 订阅 '/topic/broadcast' 目标（destination）发送的消息，
@@ -29,29 +34,33 @@ function connect() {
         // 不需要写域名（域名同上）
         // 订阅来自 @SendTo() 的消息
         stompClient.subscribe('/topic/broadcast', function (message) {
-            console.log("订阅消息：", message);
             var body = JSON.parse(message.body);
             showMessage(body.from, body.msg);
         });
 
         // 服务端 WebSocketHandler 发送的消息
         socket.onmessage = function (e) {
-            console.log("onmessage消息：", e);
-            try {
-                var data = JSON.parse(e.data);
-                var type = data.type;
-                var number = data.number;
-                if (type === "onlineMsg") {
-                    var username = data.username;
-                    var online = data.online;
-                    showOnline(online, username, number);
-                } else if (type === "onlineNum") {
-                    var users = data.users;
-                    console.log("在线用户：" + users)
+            var data = JSON.parse(e.data);
+            var type = data.type;
+            var number = data.number;
+            if (type === "onlineMsg") {
+                var username = data.username;
+                var online = data.online;
+                if (online === true) {
+                    console.log("上线用户", username)
+                } else if (online === false) {
+                    console.log("下线用户", username)
+                } else {
+                    console.log("参数 online 类型错误：", online)
                 }
-            } catch (e) {
 
+            } else if (type === "onlineNum") {
+                var users = data.users;
+                console.log("在线用户：" + users)
+            } else {
+                console.log("未知 type：", type)
             }
+            console.log("在线用户人数", number);
         };
 
     }, function (err) { // 连接异常
@@ -73,52 +82,17 @@ function showMessage(from, msg) {
 }
 
 /**
- * 显示上线、下线通知
- *
- * @param online 上线、下线
- * @param username 用户名
- * @param number 在线用户数
- */
-function showOnline(online, username, number) {
-
-    var strong = online === true ? "用户上线通知" : "用户下线通知";
-    var alert = online === true ? "alert-success" : "alert-secondary";
-    var id = new Date().getTime();
-
-    var message = "<div class=\"alert " + alert + " alert-dismissible fade show\" role=\"alert\" id=\"" + id + "\">" +
-        "<strong>" + strong + "</strong>" +
-        "用户 " + username + " 上线" +
-        "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">" +
-        "<span aria-hidden=\"true\" id=\"onlineHidden-" + id + "\">&times;</span>" +
-        "</button>" +
-        "</div>";
-
-    $("#online").append(message);
-
-    $("#number").val(number);
-
-    setTimeout(function () {
-        $("#onlineHidden-" + id).click();
-    }, 3000);
-}
-
-/**
- * 设置连接状态
+ * 设置已连接
  */
 function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
     if (connected) {
-        $("#isConnect").show();
-        $("#connect").hide();
-        $("#disconnect").show();
-        $("#online-number").show();
-
+        $("#conversation").show();
     } else {
-        $("#isConnect").hide();
-        $("#connect").show();
-        $("#disconnect").hide();
-        $("#online-number").hide();
+        $("#conversation").hide();
     }
-    // $("#message").html(""); // （不刷新页面、连接、断开时）清理历史记录
+    $("#message").html("");
 }
 
 /**
@@ -128,6 +102,10 @@ function disconnect() {
     if (stompClient !== null) {
         stompClient.disconnect();
     }
+
+    $("#isConnect").hide();
+    $("#disconnect").hide();
+    $("#connect").show();
 
     // 设置已断开连接
     setConnected(false);
@@ -155,10 +133,20 @@ function sendMessage() {
         'msg': $("#msg").val()
     }));
 
+    stompClient.onmessage = function (e) {
+        console.log('message', e.data);
+    };
+
     console.log("Stomp SockJS 已发送 - " + new Date().toLocaleTimeString())
 }
 
 $(function () {
+
+    // 发送
+    // 连接时
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
 
     // 连接
     $("#connect").on("click", function () {
